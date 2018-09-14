9e0c68efb8763ec781e1a8a24fbc47aa30ab9571
Hyeonsu Lee <incleaf@gmail.com>
2018-09-14 11:44:23 +0900

    Add markdown stylings

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -3,13 +3,13 @@ import { Switch } from 'react-router';
 import { BrowserRouter, Route } from 'react-router-dom';
 // import App from './App';
 import { TermsContainer } from './components/TermContainer';
-// import RIDIPayGeneral from './terms'
+import RIDIPayGeneral from './terms/ridiPay/general.md';
 
 export const Routes: React.SFC = () => {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Switch>
-        <Route exact={true} path="/ridi-pay/general" component={() => <TermsContainer markdownSource="asfd" />} />
+        <Route exact={true} path="/ridi-pay/general" component={() => <TermsContainer markdownSource={RIDIPayGeneral} />} />
       </Switch>
     </BrowserRouter>
   );
--- src/components/TermContainer.tsx
+++ src/components/TermContainer.tsx
@@ -2,6 +2,7 @@ import { Icon } from '@ridi/rsg';
 import { css } from 'emotion';
 import * as React from 'react';
 import * as Markdown from 'react-markdown'
+import { colors } from '../colors';
 
 export interface TermsContainerProps {
   markdownSource: string;
@@ -14,7 +15,7 @@ export const TermsContainer: React.SFC<TermsContainerProps> = (props) => {
         <Icon name="logo_ridibooks_3" className={s.ridiIcon} />
       </header>
       <main className={s.main}>
-        <Markdown source={props.markdownSource} />
+        <Markdown source={props.markdownSource} className={s.markdown} />
         <footer className={s.footer}>
           <strong>
             © RIDI Corp.
@@ -40,6 +41,9 @@ const s = {
   main: css({
     maxWidth: '767px',
     margin: '0 auto',
+    padding: '50px 30px 10px 30px',
+    background: '#fff',
+    boxSizing: 'border-box',
   }),
   footer: css({
     textAlign: 'center',
@@ -48,5 +52,103 @@ const s = {
     padding: '10px 0',
     color: '#40474d',
     fontSize: '13px',
+  }),
+  markdown: css({
+    'h1, h2, h3, h4, h5, h6': {
+      fontWeight: 'bold',
+      lineHeight: '1.3em',
+    },
+    'h1': {
+      fontSize: '26px',
+      padding: '10px 0 0 0',
+      color: colors.slategray_90,
+    },
+    'h2': {
+      fontSize: '20px',
+      padding: '35px 0 0 0',
+      color: '#000',
+      fontWeight: 'bold',
+      marginTop: '30px',
+      borderTop: '1px solid #dfdfdf',
+    },
+    'h3': {
+      fontSize: '16px',
+      padding: '20px 0 0 0',
+      color: '#373a3c',
+      fontWeight: 'normal',
+    },
+    'h4, h5, h6': {
+      fontSize: '14px',
+      color: colors.slategray_80,
+    },
+    'p, a, strong, em, li': {
+      fontSize: '13px',
+      color: colors.slategray_80,
+      lineHeight: '1.7em',
+    },
+    'p': {
+      marginTop: '10px',
+    },
+    'a': {
+      textDecoration: 'underline',
+      color: colors["@color_dodgerblue_3"],
+    },
+    'em, strong': {
+      fontWeight: 'bold',
+    },
+    'ins': {
+      color: '#0089d2',
+      textDecoration: 'none',
+    },
+    'img': {
+      border: 'none',
+      '-ms-interpolation-mode': 'bicubic',
+    },
+    'ul, ol': {
+      padding: '10px 0 10px 20px',
+      listStylePosition: 'outside',
+      li: {
+        margin: '3px 0 7px 0',
+        color: colors["@color_slategray_1"],
+        '&:first-child': {
+          marginTop: 0,
+        }
+      },
+    },
+    'ol': {
+      listStyleType: 'decimal'
+    },
+    'li > ol, li > ul': {
+      padding: '7px 0 7px 10px',
+    },
+    'ul > li': {
+      position: 'relative',
+      '&:after': {
+        content: '',
+        display: 'block',
+        width: '3px', height: '3px', background: colors["@color_slategray_5"],
+        position: 'absolute', left: '-8px', top: '9px',
+      }
+    },
+    table: {
+      width: '100%',
+      thead: {
+        background: colors["@color_slategray_8"],
+      },
+      tbody: {
+        background: colors["@color_slategray_14"],
+      },
+      'th, td': {
+        border: `1px solid ${colors["@color_slategray_10"]}`,
+        verticalAlign: 'top', padding: '10px',
+        fontSize: '13px', lineHeight: '1.7em',
+      },
+      th: {
+        textAlign: 'center', color: colors["@color_slategray_3"],
+      },
+      td: {
+        textAlign: 'left', color: colors["@color_slategray_1"],
+      }
+    }
   }),
 }
