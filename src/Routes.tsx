ab9d879b2a4c876b23aadcd4386548a68e6afc85
Hyeonsu Lee <incleaf@gmail.com>
2018-09-14 12:23:55 +0900

    Add RIDI Pay privacy route

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -4,12 +4,14 @@ import { BrowserRouter, Route } from 'react-router-dom';
 // import App from './App';
 import { TermsContainer } from './components/TermContainer';
 import RIDIPayGeneral from './terms/ridiPay/general.md';
+import RIDIPayPrivacy from './terms/ridiPay/privacy.md';
 
 export const Routes: React.SFC = () => {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route exact={true} path="/ridi-pay/general" component={() => <TermsContainer markdownSource={RIDIPayGeneral} />} />
+        <Route exact={true} path="/ridi-pay/privacy" component={() => <TermsContainer markdownSource={RIDIPayPrivacy} />} />
       </Switch>
     </BrowserRouter>
   );
