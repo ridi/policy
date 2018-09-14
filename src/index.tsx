b7c08921eea53f7995c249dcaa27c68961cf309a
Hyeonsu Lee <incleaf@gmail.com>
2018-09-14 12:12:58 +0900

    Add reset stylings

--- src/index.tsx
+++ src/index.tsx
@@ -5,10 +5,12 @@ import { Routes } from './Routes';
 
 injectGlobal({
   body: {
+    fontFamily: 'sans-serif',
+  },
+  'blockquote, body, button, code, dd, div, dl, dt, fieldset, form, h1, h2, h3, h4, h5, h6, html, iframe, input, legend, li, ol, p, pre, strong, td, textarea, th, ul': {
     margin: 0,
     padding: 0,
-    fontFamily: 'sans-serif',
-  }
+  },
 })
 
 ReactDOM.render(
